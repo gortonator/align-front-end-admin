deb67de5d12bbbba0b5175f5c710ea61e559c35a
Rohit <rohitdr786@gmail.com>
2018-04-02 00:31:50 -0400

    Reverting testing changes
    Reverting testing changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-// const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const ROOT_URL = 'http://asd2.ccs.neu.edu:8080/webapi';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+// const ROOT_URL = 'http://asd2.ccs.neu.edu:8080/webapi';
 // const ROOT_URL = '';
-const ROOT_URL_ANALYTICS = ROOT_URL + '/analytics';
+// const ROOT_URL_ANALYTICS = ROOT_URL + '/analytics';
 const API_KEY = '?key=STUDENT12345';
-const token="eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..RhkeytG5GSaoAcn7l-vjKw.cwBol4yJoF8IhzNSw5n7oKnqx38dDtt54sIKZGRBWrd5a6cDy4avH6IzGG1XfkDvDsAxnqA3wFiO83MgUfLQA25EQQazkRQr_sRFWiRxRQY.it38_7BlRQ-F4I-nBgOeMg";
+// const token="eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..RhkeytG5GSaoAcn7l-vjKw.cwBol4yJoF8IhzNSw5n7oKnqx38dDtt54sIKZGRBWrd5a6cDy4avH6IzGG1XfkDvDsAxnqA3wFiO83MgUfLQA25EQQazkRQr_sRFWiRxRQY.it38_7BlRQ-F4I-nBgOeMg";
 
 export const SEARCH_STUDENT="search_student";
 export const FETCH_STUDENT="fetch_student";
@@ -41,7 +41,8 @@ export function selectPage(page){
 
 export function getAnalytics(chartRequest, callback){
     console.log(chartRequest);
-    const request = axios.post(`${ROOT_URL_ANALYTICS}/${chartRequest.url}`,chartRequest.body,{headers:{"Content-Type":"application/json","token":token}}).then(()=>{
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`).then(()=>{
+    // const request = axios.post(`${ROOT_URL_ANALYTICS}/${chartRequest.url}`,chartRequest.body,{headers:{"Content-Type":"application/json","token":token}}).then(()=>{
         callback(chartRequest.url);
     });
     return {
