a52ffd212384884e8be663e7cc19689d4bd7c41b
Rohit <rohitdr786@gmail.com>
2018-04-06 02:04:37 -0400

    Minor Change for login
    Minor Change for login

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const ROOT_TEST = 'http://reduxblog.herokuapp.com/api';
 const ROOT_URL = 'http://asd2.ccs.neu.edu:8081';
 const ROOT_URL_ANALYTICS = ROOT_URL + '/analytics';
+const API_KEY = '?key=STUDENT12345';
 
 export const SEARCH_STUDENT="search_student";
 export const FETCH_STUDENT="fetch_student";
@@ -41,6 +42,12 @@ export function selectPage(page){
 
 
 export function getAnalytics(chartRequest, token){
+    // console.log(chartRequest);
+    // const request = axios.get(`${ROOT_URL}/posts${API_KEY}`).then(()=>{
+    // const request = axios.post(`${ROOT_URL_ANALYTICS}/${chartRequest.url}`,{"campus":"boston"},{headers:{"Content-Type":"application/json","token":token}}).then(()=>{
+    //     callback(chartRequest.url);
+    // });
+    // const request = axios.post(`http://asd2.ccs.neu.edu:8080/top-undergradschools`,{},{headers:{"Content-Type":"application/json","token":token}});
     const request = axios.post(`${ROOT_URL_ANALYTICS}/${chartRequest.url}`,chartRequest.body,{headers:{"Content-Type":"application/json","token":token}});
     return {
       type: GET_ANALYTICS,
@@ -71,13 +78,9 @@ export function clearLogin(){
   }
 }
 export function checkLogin(){
-  // console.log("In checkLogin");
   var sessionID=sessionStorage.getItem("mscs_align_neu_id");
   var sessionToken=sessionStorage.getItem("mscs_align_neu_token");
-  // console.log("sessionID",sessionID,"sessionToken",sessionToken);
   if(!(sessionID && sessionToken)){
-    // alert("Session timed out. Please login again.");
-    // console.log("redirecting");
     window.location.replace("/");
   }else{
     return{
